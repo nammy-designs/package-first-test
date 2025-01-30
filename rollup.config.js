c2dfd5e06749c64fb3ab4bd485677c37a0609860
nammy-designs <nammydesigns@gmail.com>
2025-01-30 11:43:31 +0530

    add typescript support

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
 
 export default [
   {
@@ -15,25 +14,11 @@ export default [
       },
     ],
     external: ["react", "react-dom"], // Exclude react and react-dom as peer dependencies
-    plugins: [
-      resolve(),
-      commonjs(),
-      postcss({
-        extract: true, // Extract CSS to a separate file (optional)
-        minimize: true, // Minimize the output CSS in production
-      }),
-      typescript(),
-    ],
+    plugins: [resolve(), commonjs(), typescript()],
   },
   {
     input: "dist/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "es" }],
-    plugins: [
-      dts({
-        respectExternal: true,
-        compilerOptions: { skipLibCheck: true },
-      }),
-    ],
-    external: [/\.css$/], // Ignore CSS files in declarations
+    plugins: [dts()],
   },
 ];
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import "./styles/tailwind.css";
-
 import MyComponent from "./components/MyComponent";
 
 export { MyComponent };
